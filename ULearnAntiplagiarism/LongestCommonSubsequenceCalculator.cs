9ddd8e1764abb0e268311a913a1905274af34223
Андрей Власов <andrey11vlasov@yandex.ru>
2019-04-13 22:43:00 +0300

    Add LCS solution

--- ULearnAntiplagiarism/LongestCommonSubsequenceCalculator.cs
+++ ULearnAntiplagiarism/LongestCommonSubsequenceCalculator.cs
@@ -13,12 +13,32 @@ namespace Antiplagiarism
 
         private static int[,] CreateOptimizationTable(List<string> first, List<string> second)
         {
-            throw new NotImplementedException();
+            var opt = new int[first.Count + 1, second.Count + 1];
+            for (int i = first.Count - 1; i >= 0; i--)
+                for (int j = second.Count - 1; j >= 0; j--)
+                    if (TokenDistanceCalculator.GetTokenDistance(first[i], second[j]) == 0)
+                        opt[i, j] = 1 + opt[i + 1, j + 1];
+                    else
+                        opt[i, j] = Math.Max(opt[i + 1, j], opt[i, j + 1]);
+            return opt;
         }
 
         private static List<string> RestoreAnswer(int[,] opt, List<string> first, List<string> second)
         {
-            throw new NotImplementedException();
+            var res = new List<string>();
+            for (int i = 0, j = 0; opt[i, j] != 0 && i < first.Count && j < second.Count;)
+                if (TokenDistanceCalculator.GetTokenDistance(first[i], second[j]) == 0)
+                {
+                    res.Add(first[i]);
+                    i++;
+                    j++;
+                }
+                else
+                    if (opt[i, j] == opt[i + 1, j])
+                    i++;
+                else
+                    j++;
+            return res;
         }
     }
 }
