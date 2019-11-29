4b57dae2c63176b9a8951505d2bade01267ede69
Андрей Власов <andrey11vlasov@yandex.ru>
2019-11-29 15:20:29 +0300

    Add a method with Parallel.For solution

--- ULearnRocketBot/Bot_Parallel.cs
+++ ULearnRocketBot/Bot_Parallel.cs
@@ -7,7 +7,7 @@ using System.Collections.Concurrent;
 namespace rocket_bot
 {
     public partial class Bot
-    {      
+    {
         public Rocket GetNextMove(Rocket rocket)
         {
             var moves = new ConcurrentBag<Tuple<Turn, double>>();
@@ -27,5 +27,29 @@ namespace rocket_bot
             var bestMove = moves.OrderBy(x => x.Item2).First();
             return rocket.Move(bestMove.Item1, level);
         }
+
+        // Следующее решение проходит тесты через раз =>
+
+        //public class TupleComparer : IComparer<Tuple<Turn, double>>
+        //{
+        //    public int Compare(Tuple<Turn, double> x, Tuple<Turn, double> y)
+        //        => x.Item2.CompareTo(y.Item2);
+        //}
+
+        //public Rocket GetNextMove(Rocket rocket)
+        //{
+        //    var moves = new List<Tuple<Turn, double>>();
+        //    Parallel.For(0, threadsCount, i =>
+        //    {
+        //        Random random = new Random();
+        //        var bestMoveInThread = SearchBestMove(rocket,
+        //            random, iterationsCount / threadsCount);
+        //        lock (moves)
+        //            moves.Add(bestMoveInThread);
+        //    });
+        //    moves.Sort(new TupleComparer());
+        //    var bestMove = moves.First();
+        //    return rocket.Move(bestMove.Item1, level);
+        //}
     }
 }
