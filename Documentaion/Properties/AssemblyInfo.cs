efb38c63c4fb52f228504226c7f7cfe1ebbe371f
Андрей Власов <andrey11vlasov@yandex.ru>
2019-12-04 19:59:33 +0300

    Add a documentation solution

--- Documentaion/Properties/AssemblyInfo.cs
+++ Documentaion/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Documentation")]
+[assembly: AssemblyTitle("Documentaion")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Documentation")]
+[assembly: AssemblyProduct("Documentaion")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("08787a76-f59c-4a2b-bb4f-63c00994ea56")]
+[assembly: Guid("bc155f0c-5ab9-4e87-9768-56a3839a9c5f")]
 
 // Version information for an assembly consists of the following four values:
 //
