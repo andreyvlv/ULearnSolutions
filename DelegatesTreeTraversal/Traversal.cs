8dd8d40718c582c79ac360ee8458ebdb9b50a7b9
Андрей Власов <andrey11vlasov@yandex.ru>
2020-06-28 23:18:46 +0300

    Add a Delegates Tree Traversal solution

---
+++ DelegatesTreeTraversal/Traversal.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Delegates.TreeTraversal
+{
+    public class Traversal
+    {       
+        public static IEnumerable<T> GetBinaryTreeValues<T>(BinaryTree<T> tree)
+            => GetElementsByTraverse(
+                tree,
+                t => new[] { t.Left, t.Right },
+                t => new[] { t.Value });
+
+        public static IEnumerable<Job> GetEndJobs(Job job)
+            => GetElementsByTraverse(
+                job,
+                j => j.Subjobs,
+                j => new[] { j.Subjobs.Count == 0 ? j : null });
+
+        public static IEnumerable<Product> GetProducts(ProductCategory category)
+            => GetElementsByTraverse(
+                category,
+                c => c.Categories,
+                c => c.Products);
+
+        static IEnumerable<TResult> GetElementsByTraverse<TNode, TResult>(TNode collection,
+            Func<TNode, IEnumerable<TNode>> getNodes,
+            Func<TNode, IEnumerable<TResult>> selector)
+        {
+            var list = new List<TResult>();
+            var queue = new Queue<TNode>();
+            queue.Enqueue(collection);
+            while (queue.Count > 0)
+            {
+                var node = queue.Dequeue();
+                var nodes = getNodes(node);
+                foreach (var newNode in nodes)
+                    if(newNode != null)
+                        queue.Enqueue(newNode);                
+                list.AddRange(selector(node).Where(x => x != null));
+            }
+            return list;
+        }
+    }
+}
---
+++ DelegatesTreeTraversal/TraversalOld.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates.TreeTraversal
+{
+    public class TraversalOld
+    {
+        public static IEnumerable<T> GetBinaryTreeValues<T>(BinaryTree<T> tree)
+        {
+            var result = new List<T>();
+            FillList((queue) =>
+            {
+                var node = queue.Dequeue();
+                if (node != null)
+                {
+                    queue.Enqueue(node.Left);
+                    queue.Enqueue(node.Right);
+                    result.Add(node.Value);
+                }
+            }, tree);            
+            return result;            
+        }
+
+        public static IEnumerable<Job> GetEndJobs(Job job)
+        {
+            var result = new List<Job>();
+            FillList((queue) =>
+            {
+                var node = queue.Dequeue();                
+                if (node.Subjobs.Count != 0)
+                    foreach (var subJob in node.Subjobs)
+                        queue.Enqueue(subJob);
+                else
+                    result.Add(node);
+            }, job);           
+            return result;            
+        }
+
+        public static IEnumerable<Product> GetProducts(ProductCategory category)
+        {
+            var result = new List<Product>();
+            FillList((queue) =>
+            {
+                var node = queue.Dequeue();                
+                if (node.Categories.Count != 0)               
+                    foreach (var prodCat in node.Categories)
+                        queue.Enqueue(prodCat);                
+                foreach (var prod in node.Products)                
+                    result.Add(prod);                               
+            }, category);            
+            return result;
+        }
+
+        private static void FillList<T>(Action<Queue<T>> func, T collection)
+        {           
+            var queue = new Queue<T>();            
+            queue.Enqueue(collection);
+            while (queue.Count > 0)
+                func(queue);            
+        }
+    }
+}
