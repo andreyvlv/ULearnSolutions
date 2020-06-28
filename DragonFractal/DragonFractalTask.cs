2154ea7fb1a7d4a8c281c9763491d7f982c3f3d9
Андрей Власов <andrey11vlasov@yandex.ru>
2020-06-28 23:53:08 +0300

    Add a Dragon Fractal solution

---
+++ DragonFractal/DragonFractalTask.cs
@@ -0,0 +1,38 @@
+﻿// В этом пространстве имен содержатся средства для работы с изображениями. Чтобы оно стало доступно, в проект был подключен Reference на сборку System.Drawing.dll
+using System;
+using System.Drawing;
+
+namespace Fractals
+{
+	internal static class DragonFractalTask
+	{      
+
+        public static void DrawDragonFractal(Pixels pixels, int iterationsCount, int seed)
+        {
+            double x = 1.0;
+            double y = 0.0;
+            const double angle45 = Math.PI / 4;
+            const double angle135 = Math.PI - angle45;            
+            Random rnd = new Random(seed);
+            for (int i = 0; i < iterationsCount; i++)
+            {               
+                if (rnd.Next(0, 2) == 1)
+                {
+                    var x1 = (x * Math.Cos(angle45) - y * Math.Sin(angle45)) / Math.Sqrt(2);
+                    var y1 = (x * Math.Sin(angle45) + y * Math.Cos(angle45)) / Math.Sqrt(2);
+                    x = x1;
+                    y = y1;
+                }
+                else
+                {
+                    var x1 = (x * Math.Cos(angle135) - y * Math.Sin(angle135)) / Math.Sqrt(2) + 1;
+                    var y1 = (x * Math.Sin(angle135) + y * Math.Cos(angle135)) / Math.Sqrt(2);
+                    x = x1;
+                    y = y1;
+                }               
+                pixels.SetPixel(x, y);
+            }
+        }       
+
+    }
+}
